1e2705e6288dcfaf967703ee7e3337829016c887
munemo <john.chimbani@outlook.com>
2020-08-19 14:25:57 +0200

    Second test is passing

--- cypress/integration/userCanConnectToHomepage.feature.js
+++ cypress/integration/userCanConnectToHomepage.feature.js
@@ -4,4 +4,8 @@ describe('user can connect to homepage', () => {
 		cy.visit('http://localhost:3001')
 		cy.get('#quote').click()
 	})
+
+	it('checks for greeting', () => {
+		cy.get('#message').should('contain', "hello how are you")
+	})
 })
