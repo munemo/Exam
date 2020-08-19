92a283150bd3ee5bbdf51e9db928812aecc61411
munemo <john.chimbani@outlook.com>
2020-08-19 15:24:58 +0200

    Missing tests

--- cypress/integration/userCanConnectToHomepage.feature.js
+++ cypress/integration/userCanConnectToHomepage.feature.js
@@ -1,9 +1,14 @@
 
 describe('user can connect to homepage', () => {
-	it('test', () => {
+	it('test connectivity to localhost', () => {
 		cy.visit('http://localhost:3001')
-		cy.get('#quote').click()
+		
 	})
+
+	it('tests button functionality', () => {
+		cy.get('#qoute').click()
+	})
+
 
 	it('checks for greeting', () => {
 		cy.get('#message').should('contain', "hello how are you")
