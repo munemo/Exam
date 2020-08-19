22723886733d122dacfaf2245516c94fcfb429c0
munemo <john.chimbani@outlook.com>
2020-08-19 14:39:15 +0200

    All three tests are passing

--- cypress/integration/userCanConnectToHomepage.feature.js
+++ cypress/integration/userCanConnectToHomepage.feature.js
@@ -1,11 +1,16 @@
 
-describe('user can connect to homepage', () => {
-	it('test', () => {
+describe('Exam application', () => {
+	it('tests connectivity to local host', () => {
 		cy.visit('http://localhost:3001')
+		
+	})
+
+	it('test button functionality when clicked', () => {
 		cy.get('#quote').click()
 	})
 
-	it('checks for greeting', () => {
+
+	it('test for greeting after button is clicked', () => {
 		cy.get('#message').should('contain', "hello how are you")
 	})
 })
